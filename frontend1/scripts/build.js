3b5a842623973c657ebfd2dbb45b0e006aa7babe
ash <akashkulal866087@gmail.com>
2026-07-03 13:13:12 +0530

    fix: clean up build scripts, use plain react-scripts build

--- frontend1/scripts/build.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require("child_process");
-process.env.NODE_OPTIONS = "--openssl-legacy-provider";
-process.env.GENERATE_SOURCEMAP = "false";
-execSync("npx react-scripts build", { stdio: "inherit" });
