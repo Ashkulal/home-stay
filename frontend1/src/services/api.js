75193e4f10d544db1a4f68bf77a97ee8f9fd86db
ash <akashkulal866087@gmail.com>
2026-07-23 17:24:48 +0530

    fix: hardcode Render API URL, deploy from frontend1

--- frontend1/src/services/api.js
+++ frontend1/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://home-stay-mh8a.onrender.com/api",
+  baseURL: "https://home-stay-mh8a.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
